37028bf454a0bcf6469550d77d979b7cd86c4413
Ahmed Mashhour <mpghknown@gmail.com>
2021-05-10 12:49:38 +0200

    change account id migratE

--- db/migrate/20171201000000_change_account_id_nonnullable_in_lists.rb
+++ db/migrate/20171201000000_change_account_id_nonnullable_in_lists.rb
@@ -1,5 +1,19 @@
-class ChangeAccountIdNonnullableInLists < ActiveRecord::Migration[5.1]
+class ChangeAccountIdNonnullableInLists < ActiveRecord::Migration[5.2]
   def change
+    safety_assured do
+      execute 'ALTER TABLE "lists" ADD CONSTRAINT "lists_account_id_null" CHECK ("account_id" IS NOT NULL) NOT VALID'
+    end
+  end
+end
+
+class ValidateChangeAccountIdNonnullableInLists < ActiveRecord::Migration[5.2]
+  def change
+    safety_assured do
+      execute 'ALTER TABLE "lists" VALIDATE CONSTRAINT "lists_account_id_null"'
+    end
     change_column_null :lists, :account_id, false
+    safety_assured do
+      execute 'ALTER TABLE "lists" DROP CONSTRAINT "lists_account_id_null"'
+    end
   end
 end
