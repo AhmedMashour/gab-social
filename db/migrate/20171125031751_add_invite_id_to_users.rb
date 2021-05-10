b53b821b02e05ffa75611185c835c2b54e4fd7d7
Ahmed Mashhour <mpghknown@gmail.com>
2021-05-10 12:48:35 +0200

    modifying migration

--- db/migrate/20171125031751_add_invite_id_to_users.rb
+++ db/migrate/20171125031751_add_invite_id_to_users.rb
@@ -1,5 +1,12 @@
-class AddInviteIdToUsers < ActiveRecord::Migration[5.1]
+# class AddInviteIdToUsers < ActiveRecord::Migration[5.1]
+#   def change
+#     add_reference :users, :invite, null: true, default: nil, foreign_key: { on_delete: :nullify }, index: false
+#   end
+# end
+class AddInviteIdToUsers < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
   def change
-    add_reference :users, :invite, null: true, default: nil, foreign_key: { on_delete: :nullify }, index: false
+    add_reference :users, :invite, null: true, default: nil, index: {algorithm: :concurrently}
   end
 end
