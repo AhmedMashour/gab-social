57a2e22f80fb9a2a815b69d5d6e97cd00a1d1ba5
Ahmed Mashhour <mpghknown@gmail.com>
2021-05-10 12:43:33 +0200

    modifying migration

--- db/migrate/20171010025614_change_accounts_nonnullable_in_account_moderation_notes.rb
+++ db/migrate/20171010025614_change_accounts_nonnullable_in_account_moderation_notes.rb
@@ -4,3 +4,23 @@ class ChangeAccountsNonnullableInAccountModerationNotes < ActiveRecord::Migratio
     change_column_null :account_moderation_notes, :target_account_id, false
   end
 end
+
+class ChangeAccountsNonnullableInAccountModerationNotes < ActiveRecord::Migration[5.2]
+  def change
+    safety_assured do
+      execute 'ALTER TABLE "account_moderation_notes" ADD CONSTRAINT "account_moderation_notes_account_id_null" CHECK ("account_id" IS NOT NULL) NOT VALID'
+    end
+  end
+end
+
+class ValidateChangeAccountsNonnullableInAccountModerationNotes < ActiveRecord::Migration[5.2]
+  def change
+    safety_assured do
+      execute 'ALTER TABLE "account_moderation_notes" VALIDATE CONSTRAINT "account_moderation_notes_account_id_null"'
+    end
+    change_column_null :account_moderation_notes, :account_id, false
+    safety_assured do
+      execute 'ALTER TABLE "account_moderation_notes" DROP CONSTRAINT "account_moderation_notes_account_id_null"'
+    end
+  end
+end
