d224ce8533beb7df0d920e85d4e7da0844b95a3b
Ahmed Mashhour <mpghknown@gmail.com>
2021-05-10 13:25:19 +0200

    all migration report

--- db/migrate/20180402040909_create_report_notes.rb
+++ db/migrate/20180402040909_create_report_notes.rb
@@ -1,4 +1,12 @@
-class CreateReportNotes < ActiveRecord::Migration[5.1]
+# class CreateReportNotes < ActiveRecord::Migration[5.1]
+#   def change
+   
+
+#     add_foreign_key :report_notes, :reports, column: :report_id, on_delete: :cascade
+#     add_foreign_key :report_notes, :accounts, column: :account_id, on_delete: :cascade
+#   end
+# end
+class CreateReportNotes < ActiveRecord::Migration[5.2]
   def change
     create_table :report_notes do |t|
       t.text :content, null: false
@@ -8,7 +16,13 @@ class CreateReportNotes < ActiveRecord::Migration[5.1]
       t.timestamps
     end
 
-    add_foreign_key :report_notes, :reports, column: :report_id, on_delete: :cascade
-    add_foreign_key :report_notes, :accounts, column: :account_id, on_delete: :cascade
+    add_foreign_key :report_notes, :reports, column: :report_id, on_delete: :cascade, validate: false
+  end
+end
+
+class ValidateCreateReportNotes < ActiveRecord::Migration[5.2]
+  def change
+    validate_foreign_key :report_notes, :reports
+    validate_foreign_key :report_notes, :accounts
   end
 end
