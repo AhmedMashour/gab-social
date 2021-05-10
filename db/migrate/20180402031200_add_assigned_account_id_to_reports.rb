c7d42744cc6015e0c06ba6aeb8d3d08ae6d12f01
Ahmed Mashhour <mpghknown@gmail.com>
2021-05-10 13:20:34 +0200

    all migration ddl

--- db/migrate/20180402031200_add_assigned_account_id_to_reports.rb
+++ db/migrate/20180402031200_add_assigned_account_id_to_reports.rb
@@ -1,5 +1,7 @@
-class AddAssignedAccountIdToReports < ActiveRecord::Migration[5.1]
+class AddAssignedAccountIdToReports < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
   def change
-    add_reference :reports, :assigned_account, null: true, default: nil, foreign_key: { on_delete: :nullify, to_table: :accounts }, index: false
+    add_reference :reports, :assigned_account, null: true, default: nil, index: {algorithm: :concurrently}
   end
 end
