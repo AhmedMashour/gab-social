bfb2e9b8328ff8d1de7c79aaf73ea428968b3c3f
Ahmed Mashhour <mpghknown@gmail.com>
2021-05-10 13:29:08 +0200

    all migration report

--- db/migrate/20180510214435_add_access_token_id_to_web_push_subscriptions.rb
+++ db/migrate/20180510214435_add_access_token_id_to_web_push_subscriptions.rb
@@ -1,6 +1,15 @@
+# class AddAccessTokenIdToWebPushSubscriptions < ActiveRecord::Migration[5.2]
+#   def change
+#     add_reference :web_push_subscriptions, :access_token, null: true, default: nil, foreign_key: { on_delete: :cascade, to_table: :oauth_access_tokens }, index: false
+#     add_reference :web_push_subscriptions, :user, null: true, default: nil, foreign_key: { on_delete: :cascade }, index: false
+#   end
+# end
+
 class AddAccessTokenIdToWebPushSubscriptions < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
   def change
-    add_reference :web_push_subscriptions, :access_token, null: true, default: nil, foreign_key: { on_delete: :cascade, to_table: :oauth_access_tokens }, index: false
-    add_reference :web_push_subscriptions, :user, null: true, default: nil, foreign_key: { on_delete: :cascade }, index: false
+    add_reference :web_push_subscriptions, :access_token, null: true, default: nil, index: {algorithm: :concurrently}
+    add_reference :web_push_subscriptions, :user, null: true, default: nil, index: {algorithm: :concurrently}
   end
 end
